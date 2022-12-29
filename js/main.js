542b682124b1bbd0e789ff6ba19aa3edcc4bd80b
Fede Villarreal <fedevillarreal94@gmail.com>
2022-12-28 23:22:19 -0300

    Agregue un modo Oscuro, y permite que quede guardado en el LocalStore, para que se conserven los cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Funcion para insertar las tarjetas al HTML desde JS */
+
 function cardAHtml (array) {
     const contenedor = document.querySelector(".contenedor")
     array.forEach ( (element) => {
@@ -16,21 +18,23 @@ function cardAHtml (array) {
 }
 cardAHtml(wildRiftChampions)
 
+
+/* Funcion que retorna una lista filtrada de Champios de acuerdo a su rol */
+
 const filtrarPorRol = ( array, posicion ) => {
     return array.filter ( ( champion ) => {
         for (let i = 0; i < champion.rol.length; i++) {
-            if (champion.rol[i] == posicion){
+            if (posicion == "All Lane") {
+                return champion.rol
+            } else if (champion.rol[i] == posicion) {
                 return champion.rol
             }
         }
     } )
 }
 
-console.log(filtrarPorRol(wildRiftChampions, "Solo Lane"))
-console.log(filtrarPorRol(wildRiftChampions, "Mid Lane"))
-console.log(filtrarPorRol(wildRiftChampions, "Dragon Lane"))
-console.log(filtrarPorRol(wildRiftChampions, "Support"))
-console.log(filtrarPorRol(wildRiftChampions, "Jungle"))
+
+/* Funcion para remover las tarjetas antiguas (se utiliza dentro de la funcion filtrar por rol) */
 
 const removeCards = () => {
     let allCards = document.querySelectorAll(".card")
@@ -39,38 +43,70 @@ const removeCards = () => {
     } )
 }
 
+
+/* Declaracion de variables DOM de acuerdo al Rol de los Champions */
+
 const allLane = document.querySelector("#all-lane")
-allLane.onclick = () => {
-    removeCards()
-    cardAHtml(wildRiftChampions)
-}
-
 const soloLane = document.querySelector("#solo-lane")
-soloLane.onclick = () => {
-    removeCards()
-    cardAHtml( filtrarPorRol ( wildRiftChampions, "Solo Lane" ) )
-}
-
 const midLane = document.querySelector("#mid-lane")
-midLane.onclick = () => {
-    removeCards()
-    cardAHtml( filtrarPorRol ( wildRiftChampions, "Mid Lane" ) )
-}
-
 const dragonLane = document.querySelector("#dragon-lane")
-dragonLane.onclick = () => {
-    removeCards()
-    cardAHtml( filtrarPorRol ( wildRiftChampions, "Dragon Lane" ) )
-}
-
 const support = document.querySelector("#support")
-support.onclick = () => {
-    removeCards()
-    cardAHtml( filtrarPorRol ( wildRiftChampions, "Support" ) )
+const jungle = document.querySelector("#jungle")
+
+
+/* Funcion onclick para filtar por rol (primero remueve las tarjetas anterior y luego inserta los Champios de acuerdo a su rol) */
+
+const eventoFiltrarPorRol = ( variable, rolString ) => {
+    variable.onclick = () => {
+        removeCards ()
+        cardAHtml ( filtrarPorRol ( wildRiftChampions, rolString ) )
+    }
+}
+eventoFiltrarPorRol ( allLane, "All Lane" )
+eventoFiltrarPorRol ( soloLane, "Solo Lane" )
+eventoFiltrarPorRol ( midLane, "Mid Lane" )
+eventoFiltrarPorRol ( dragonLane, "Dragon Lane" )
+eventoFiltrarPorRol ( support, "Support" )
+eventoFiltrarPorRol ( jungle, "Jungle" )
+
+
+/* Modo Claro / Modo Oscuro */
+
+const botonModosClaroOscuro = document.querySelector("#boton--claro__oscuro")
+const body = document.querySelector(".modo--claro")
+const nav = document.querySelector("nav")
+
+/* Funcion para cambiar los modos (es asignado posteriormente a una funcion onclick y a una funcion que evalua info del LocalStore al iniciar o reiniciar la pagina) */
+function cambiarModoClaroOscuro () {
+    body.classList.toggle ( "modo--oscuro" )
+    botonModosClaroOscuro.classList.toggle ("boton__modo--oscuro")
+    if ( nav.getAttribute ( "data-bs-theme" ) == "" ) {
+        nav.setAttribute ( "data-bs-theme", "dark" )
+    } else {
+        nav.setAttribute ( "data-bs-theme", "" )
+    }
 }
 
-const jungle = document.querySelector("#jungle")
-jungle.onclick = () => {
-    removeCards()
-    cardAHtml( filtrarPorRol ( wildRiftChampions, "Jungle" ) )
+/* Funcion para evaluar la key "modoOscuro" en el LocalStore */
+const evaluarModoOscuro = () => {
+    return localStorage.getItem ( "modoOscuro" )
 }
+
+/* Funcion evento que cambia los modos claro/oscuro y modifica la info del LocalStore previa una evaluación */
+botonModosClaroOscuro.onclick = () => {
+    cambiarModoClaroOscuro()
+    if ( evaluarModoOscuro() == "on" ) {
+        localStorage.setItem ( "modoOscuro", "off" )
+    } else {
+        localStorage.setItem ( "modoOscuro", "on" )
+    }
+}
+
+/* Funcion que se ocupa de evaluar al cargar la pagina si quedo guardado el modo oscuro, de ser asi lo vuelve a aplicar */
+function modoClaroOscuroInicial () {
+    if ( evaluarModoOscuro() == "on" ) {
+        cambiarModoClaroOscuro()
+    }
+}
+modoClaroOscuroInicial()
+
